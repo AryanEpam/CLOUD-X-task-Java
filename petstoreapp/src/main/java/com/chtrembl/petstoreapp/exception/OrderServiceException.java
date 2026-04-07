baa5f4f0762874f7d732ecc40bf20677c85decbd
Aryan-A <aryan_a@epam.com>
2026-04-07 16:12:53 +0530

    Order successful

--- petstoreapp/src/main/java/com/chtrembl/petstoreapp/exception/OrderServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.chtrembl.petstoreapp.exception;
-
-public class OrderServiceException extends RuntimeException {
-    public OrderServiceException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
